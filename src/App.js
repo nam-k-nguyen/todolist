2f76971b969263e206dfdc32aef4f04fbf0854f2
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-10 02:08:28 -0400

    Update App.js
    Add buttons and inputs to interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState, useRef } from 'react';
 import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(event) {
+  }
+
   return (
     <>
-      <TodoList />
-      
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Todos</button>
+      <div>0 left to do</div>
     </>
   );
 }
