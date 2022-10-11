ad018428e6410c7a0e674274e27c6a2362d1862e
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-11 09:27:30 -0400

    Update TodoList.js
    Import stylesheet and create a container for all of the Todo components

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
 import Todo from './Todo';
+import './css/TodoList.css';
 
 export default function TodoList({ todos, toggleTodo }) {
   return (
-    todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo}/>
-    })
+    <div className='list_container'>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+      })}
+    </div>
   )
 }
