9d546f83c76e2626f3e1b5d573efcc28dcb8efcf
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-10 23:28:23 -0400

    Update Todo.js
    Import Todo.css and refactor code for interface

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './css/Todo.css';
 
 export default function Todo({ todo, toggleTodo }) {
     function handleTodoClick(event) {toggleTodo(todo.id)}
 
     return (
-        <div>
-            <label>
+        <div className='container'>
+            <div className='checkbox'>
                 <input type="checkbox" checked={todo.complete} onChange={handleTodoClick}/>
-                {todo.name}
-            </label>
+            </div>
+            <div className='name'>{todo.name}</div>
         </div>
     )
 }
