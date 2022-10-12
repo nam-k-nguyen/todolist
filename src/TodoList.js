6c76be9955cc508279b93ba654a0a53f472ff4a8
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-12 15:37:52 -0400

    Update TodoList.js
    Shorten the mapping function and change the container's className

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,10 +4,8 @@ import './css/TodoList.css';
 
 export default function TodoList({ todos, toggleTodo }) {
   return (
-    <div className='list_container'>
-      {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />
-      })}
+    <div className='todo_list_container'>
+      {todos.map((todo) => <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />)}
     </div>
   )
 }
