045c383a233a1215c4d617d52f83859fd6306dd2
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-12 15:45:25 -0400

    Update App.js
    Update the handleAddTodo and handleClearTodo functions to include the new description fields and refactor them according to the new reorganization of components

--- src/App.js
+++ src/App.js
@@ -28,27 +28,32 @@ function App() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
-  function handleAddTodo(event) {
-    const name = todoNameRef.current.value;
-    if (name === '') return
-    setTodos(currentTodo => [...currentTodo, createTodo(name)])
-    todoNameRef.current.value = null;
   // 
   // FUNCTIONS 
   // 
 
   // Function to add a new todo given the name and description
+  function handleAddTodo() {
+    const name = document.querySelector('.new_name');
+    const desc = document.querySelector('.new_desc');
+    const name_val = name.value;
+    const desc_val = desc.value;
+    if (name_val === '') return
+    setTodos(currentTodo => [...currentTodo, createTodo(name_val, desc_val)])
+    name.value = null;
+    desc.value = null;
   }
 
-  
-  function todoLeft() {return todos.filter(todo => !todo.complete)}
-  
-  function createTodo(todoName) {return {id: uuidv4(), name: todoName, complete: false}}
   // Function to clear the finished todos
   function handleClearTodo() { setTodos(todoLeft()) }
 
   // Function to return the number of uncleared todos
+  function todoLeft() { return todos.filter(todo => !todo.complete) }
+
   // Function to generate the todo object given name and description
+  function createTodo(todoName, todoDesc = '') {
+    return { id: uuidv4(), name: todoName, desc: todoDesc, complete: false }
+  }
 
   // Function to change the complete state of a todo given its id
   function toggleTodo(id) {
